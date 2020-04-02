c385338874de1184f85b5589dd2b975522b8ed64
rpeschke <rp40@hawaii.edu>
2020-04-01 19:06:37 -1000

    more examples

--- ex2.py
+++ ex2.py
@@ -1,6 +1,27 @@
-import argg_hdl as ah
-import argg_hdl.examples as ahe 
+from argg_hdl import *
+from  argg_hdl.examples import *
 
 
-tb = ahe.InputDelay_tb()
-ah.convert_to_hdl(tb, "pyhdl_waveform")
+#tb3 = InputDelay_tb()
+#convert_to_hdl(tb3, "pyhdl_waveform")
+
+class tb(v_entity):
+    def __init__(self):
+        super().__init__(srcFileName=__file__)
+        self.architecture()
+
+    @architecture
+    def architecture(self):
+        clkgen = v_create(clk_generator())
+        data = v_slv(32,5)
+        @rising_edge(clkgen.clk)
+        def proc():
+            data << data + 1
+           
+
+        end_architecture()
+
+
+tb1 = v_create(tb())
+
+run_simulation(tb1, 100000 ,"test.vcd")
