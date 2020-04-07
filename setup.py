0bca5ddf0124fe8cf80f29da4ae9ad3e5c869a06
rpeschke <rp40@hawaii.edu>
2020-04-06 18:08:55 -1000

    added dependencies

--- setup.py
+++ setup.py
@@ -18,5 +18,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+          'pyvcd',
+    ],
     python_requires='>=3.6',
 )
