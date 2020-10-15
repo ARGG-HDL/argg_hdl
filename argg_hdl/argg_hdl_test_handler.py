3c9c046b51698c14cda5a4a86312a52592a6b290
rpeschke <rp40@hawaii.edu>
2020-10-14 20:55:19 -1000

    added test_duration to monitor the time it takes to run the test cases

--- argg_hdl/argg_hdl_test_handler.py
+++ argg_hdl/argg_hdl_test_handler.py
@@ -1,5 +1,5 @@
 import time
-
+from datetime import datetime
 gtests = {}
 
 def run_test(TestName):
@@ -20,6 +20,12 @@ def run_all_tests():
     endTime = time.time()
     print('  <time value=' + str(endTime- startTime) +'/>')
     print('</tests>')
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d  %H:%M:%S")
+    file1 = open("tests/test_duration.txt", "a") 
+    file1.write(current_time +"; " + str(endTime- startTime)+"\n") 
+    file1.close() 
+
 
 def add_test(TestName, testFunction):
     gtests[TestName] = testFunction
